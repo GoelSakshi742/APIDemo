6b04c18c5ad5f4c5ffd44a8d646e192692b76d41
Sakshi <Sakshi@MacBook-Air.local>
2026-03-30 14:50:23 -0600

    Switch to in-memory database for cloud deployment

--- APIDemo/Program.cs
+++ APIDemo/Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<ToyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<ToyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ToyDbContext>(options => options.UseInMemoryDatabase("ToyDb"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
